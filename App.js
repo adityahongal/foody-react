4506191ebc9a779e503a5808802beefbfb8a1111
adityahongal <adityahongalwork@gmail.com>
2024-10-29 14:35:14 +0530

    Added body and plain cards

--- App.js
+++ App.js
@@ -20,10 +20,37 @@ const Header = () => {
     )
 }
 
+const RestaurantCard = () => {
+    return (
+        <div className="res-card">
+            <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/lufqwcvjqllfw24ab82x" alt="res-logo"/>
+            <h3>Mani's Dum Biriyani</h3>
+            <h4>Biriyani, South Indian, Asian</h4>
+            <h4>4.4 stars</h4>
+            <h4>29 minutes</h4>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+        </div>
+    )
+}
+
 const AppLayout = () => {
     return (
         <div className="app">
             <Header/>
+            <Body/>
         </div>
     )
 }
